6d4d55794b138f5b6a2bb7a528598cb373b611bd
bGZo <bgzo@outlook.com>
2023-01-31 15:19:53 +0800

    feat: add post proof

---
+++ assets/proof.py
@@ -0,0 +1,35 @@
+import os
+import re
+
+def proof(path):
+  with open(path, 'r') as f:
+    lines = f.readlines()
+    print('Read ' + path + ' successfully')
+
+  with open(path, 'w' ) as f:
+    for line in lines:
+      line = re.sub(r'\[(.*?)\|(.*?)\]', '[\g<1>-\g<2>]', line)
+
+      # Half width characters
+      line = re.sub(r'([\u4e00-\u9fa5]), ([\u4e00-\u9fa5])', '\g<1>，\g<2>', line)
+      line = re.sub(r'([\u4e00-\u9fa5]). ([\u4e00-\u9fa5])', '\g<1>。\g<2>', line)
+      line = re.sub(r'([\u4e00-\u9fa5]); ([\u4e00-\u9fa5])', '\g<1>；\g<2>', line)
+      line = re.sub(r'([\u4e00-\u9fa5]) "([\u4e00-\u9fa5]+)" ', '\g<1>“\g<2>”', line)
+
+      # Logseq
+      line = re.sub(r'  \n', '\n', line)
+      line = re.sub(r'\[([A-Z0-9_]+)\]', '[^\g<1>]', line)
+
+      # URL via: https://stackoverflow.com/questions/3809401
+      line = re.sub(r'([^\(\<])(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)', '\g<1><\g<2>>', line)
+      line = re.sub(r'^(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)', '<\g<1>>', line)
+
+      f.write(line)
+
+if __name__ == '__main__':
+  target = r'_posts/'
+
+  for root, dirs, files in os.walk(target):
+    for article_name in files:
+      article_path = root + '/' + article_name
+      proof(article_path)
---
+++ tests/proof-test.py
@@ -0,0 +1,4 @@
+# TODO: 
+# refactor proof to
+# 1. support commandline using;
+# 2. custom input and output to test better;
