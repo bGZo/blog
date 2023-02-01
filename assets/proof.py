d6b7a5a01a4004f35d7045e3d38a389218f46edf
bGZo <bgzo@outlook.com>
2023-02-01 23:51:53 +0800

    proof: only keep ,&. regex

--- assets/proof.py
+++ assets/proof.py
@@ -8,18 +8,9 @@ def proof(path):
 
   with open(path, 'w' ) as f:
     for line in lines:
-      line = re.sub(r'\[(.*?)\|(.*?)\]', '[\g<1>-\g<2>]', line)
-
       # Half width characters
       line = re.sub(r'([\u4e00-\u9fa5])\, ([\u4e00-\u9fa5])', '\g<1>，\g<2>', line)
       line = re.sub(r'([\u4e00-\u9fa5])\. ([\u4e00-\u9fa5])', '\g<1>。\g<2>', line)
-
-      # Logseq
-      line = re.sub(r'  \n', '\n', line)
-
-      # URL via: https://stackoverflow.com/questions/3809401
-      line = re.sub(r'( )(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)([;, ])', '\g<1><\g<2>>\g<3>\g<4>', line)
-      line = re.sub(r'^(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)', '<\g<1>>', line)
 
       f.write(line)
 
